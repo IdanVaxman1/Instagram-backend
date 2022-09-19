a0703a1af49792c4fd8e239b3cc3fb78b38e17d3
Idan Vaxman <idanvaxman@icloud.com>
2022-09-19 17:24:44 +0300

    User profile

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 
 import User from './auth.model.js'
 
+import PostMessage from '../post/post.model.js';
+
 export const signin = async (req, res) => {
 
     const { email, password } = req.body
@@ -17,7 +19,7 @@ export const signin = async (req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
         if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid password" })
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1hr' })
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '24hr' })
 
         // console.log(token);
 
@@ -42,7 +44,7 @@ export const signup = async (req, res) => {
 
         const result = await User.create({ email: email, fullName: fullName, password: hashedPassword })
 
-        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '1hr' })
+        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '24hr' })
 
         console.log(result, token);
 
@@ -57,6 +59,22 @@ export const signup = async (req, res) => {
 
 }
 
+export const getUser = async (req, res) => {
+
+    const { id: _id } = req.params
+
+    try {
+
+        const user = await User.findById(_id)
+        res.status(200).json(user)
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
 export const updateUser = async (req, res) => {
 
     const { id: _id } = req.params
@@ -65,9 +83,24 @@ export const updateUser = async (req, res) => {
 
 
     const updatedUser = await User.findByIdAndUpdate(_id, { ...user, _id }, { new: true })
-    console.log(updatedUser );
+    console.log(updatedUser);
 
     res.status(200).json({ result: updatedUser, token })
+
+}
+
+export const getUserPosts  = async (req, res) => {
+
+    const { id: _id } = req.params
+
+    try {
+        const posts = await PostMessage.find({creator : _id})
+        console.log(posts);
+        res.status(200).json(posts)
+
+    } catch (error) {
+        console.log(error);
+    }
 
 
 }
--- api/auth/auth.routes.js
+++ api/auth/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { signin, signup , updateUser } from './auth.controller.js'
+import { signin, signup, updateUser, getUser, getUserPosts } from './auth.controller.js'
 
 import auth from '../middlewere/auth.js'
 
@@ -11,6 +11,8 @@ const router = express.Router()
 router.post('/signin', signin)
 router.post('/signup', signup)
 router.patch('/setting/:id', auth, updateUser)
+router.get('/user/:id', auth, getUser)
+router.get('/userPost/:id', auth, getUserPosts)
 
 
 export default router
